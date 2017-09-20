b955d2a27f7dccc25f5236d3a25c9ff94bfe96f7
Hime0698 <jakesennett@me.com>
2017-09-20 17:08:54 -0500

    class stuff final add

--- InClassPrograms/Game of Life/gol.java
+++ InClassPrograms/Game of Life/gol.java
@@ -36,15 +36,41 @@ class gol
 
   }
 
-  public static void printArray(boolean [][] arr)
+  public static boolean [][] compute(boolean [][] board)
   {
+    newboard = new boolean[board.length][board.length];
+
+    int neighbours;
+
+    for(int r = 1; r < board.length -1; t++)
+    {
+      for(int c = 1; c < board.length -1; c++)
+      {
+        neighbours = 0;
+        for(int i = r-1; i <= r+1; i++)
+          for(int j = c-1; j <= c+1; j++)
+            if(board[i][j] && !(r==i && j==c))
+              neighbours++;
+
+        if(board[r][c])
+          newboard[r][c] = (neighbours == 2 || neighbours == 3);
+        else
+          newboard[r][c] = (neighbours == 3);
+      }
+      }
+    }
+  }
+
+  public static void printArray(boolean [][] arr, int gen)
+  {
+    System.out.println("Gen = " + gen);
     for(int i = 0; i < arr.length; i++)
     {
       for(int j = 0; j < arr[i].length; j++)
       {
-        System.out.println(arr[i][j]);
+        System.out.print(arr[i][j] ? "* " : "  ");
+        System.out.println();
       }
-      System.out.println();
     }
   }
 }
