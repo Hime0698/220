aa618be391b0136c7edd61108caceaf37e7eaa7e
Jacob Sennett <jakesennett@me.com>
2017-09-27 16:09:02 -0500

    commentafying

--- Homework/Project Gourd 2/numberSquare.java
+++ Homework/Project Gourd 2/numberSquare.java
@@ -1,10 +1,18 @@
+/* ***************************************************
+ * Jacob Sennett && Tristan Williams
+ * 9-27-17
+ * List
+ *
+ * Program that calculates the greatest sum of 4 digits on a line in a square
+ *************************************************** */
+
 import java.io.*;
 import java.util.ArrayList;
 import java.util.Scanner;
 
 class numberSquare
 {
-    public static ArrayList<Integer> numbers = new ArrayList<Integer>();
+    public static ArrayList<Integer> numbers = new ArrayList<Integer>(); //ArrayList to store the integers read from the file
 
     public static void main(String [] args)
     {
@@ -12,7 +20,7 @@ class numberSquare
        calculate();
     }
 
-    public static void scan()
+    public static void scan() //function to scan the file and add integers to an ArrayList
     {
         Scanner sc = new Scanner(System.in);
 
@@ -22,15 +30,15 @@ class numberSquare
         }
     }
 
-    public static void calculate()
+    public static void calculate() //function to calculate the greatest four consecutive numbers
     {
         int greatest = 0;
-        for(int a = 0; a < 380; a+=20 )
+        for(int a = 0; a < 380; a+=20 ) //loop that iterates to the next line
         {
             int i = a;
-            for(int b = a; b <= i + 16; b++)
+            for(int b = a; b <= i + 16; b++) //loop that iterates through the line
             {
-                int sum = numbers.get(b) + numbers.get(b + 1) + numbers.get(b + 2) + numbers.get(b + 3);
+                int sum = numbers.get(b) + numbers.get(b + 1) + numbers.get(b + 2) + numbers.get(b + 3); //determines the sum
                 if(sum > greatest)
                     greatest = sum;
             }
